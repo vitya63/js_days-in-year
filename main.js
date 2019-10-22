bd00445aaad358f706f47fee3e75a15ea868a1f1
Viktor Lukovets <nuerio63@gmail.com>
2019-10-22 16:51:26 +0300

    solution

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 'use strict'
 
 function isLeapYear(year) {
-    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+  return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
 }
 
 function daysInYear(year) {
-    try {
-        if (!Number.isInteger(year)) {
-            throw new Error();
-        }
-        return isLeapYear(year) ? 366 : 365;
-    } catch {
-        return 'exception';
-    }
+	try {
+		if (!Number.isInteger(year)) {
+			throw new Error();
+		}
+		return isLeapYear(year) ? 366 : 365;
+	} catch {
+		return 'exception';
+	}
 }
